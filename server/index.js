602da37e247c618946e74d9880e6f7f6a3e5bd79
Suejung Shin <suejungshin@Suejungs-MacBook-Pro.local>
2019-12-12 13:56:50 -0800

    Connect with Docker

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const Earnings = require('../database/Earning.js');
 const app = express();
 const port = 4200;
-const bodyParser = require('body-parser');
-const Earnings = require('../database/Earning.js');
-const cors = require('cors');
+const mongoUrl = 'mongodb://database:27017/earnings';
 
 app.use(cors());
 app.use('/', express.static('public'));
@@ -12,13 +14,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.listen(port, () => {
   console.log(`Hello, I am listening on port ${port}`);
+  mongoose.connect(mongoUrl, {server: {reconnectTries: Number.MAX_VALUE}});
 });
 
 app.use('/stocks/:ticker', (req, res, next) => {
-  console.log(req.params)
   Earnings.find({ ticker: req.params.ticker })
     .exec(
       (err, data) => {
+        console.log('server data:', data);
         res.status(200).send(data);
       });
 });
@@ -27,6 +30,7 @@ app.get('/api/earnings', (req, res) => {
 
   Earnings.find({ ticker: req.query.ticker }).exec(
     (err, data) => {
+      console.log('server data:', data);
       res.status(200).send(data);
     });
 
